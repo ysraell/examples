cd219ec6fa79100141919bb883e0add479893aa9
Israel de Oliveira <israel.de.oliveira@hp.com>
2022-12-05 17:31:01 -0300

    wip

--- Streamlit_DEMO/Text_input.py
+++ Streamlit_DEMO/Text_input.py
@@ -2,3 +2,14 @@ import streamlit as st
 
 title = st.text_input("Search:", "")
 st.write("You search for: ", title)
+
+
+import time
+
+with st.empty():
+     for seconds in range(60):
+         st.write(f"⏳ {seconds} seconds have passed")
+         time.sleep(1)
+     st.write("✔️ 1 minute over!")
+
+     
